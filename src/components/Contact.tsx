0f40fd676f3e3da9bc4666a5fb085c2e05429a15
Jesse Tran <jessetran03@gmail.com>
2021-12-30 23:23:27 -0600

    css changes

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,11 +5,16 @@ const Contact: FC = () => {
     <section id="contact" className="contact">
       <h2>Contact</h2>
       <form action="https://formspree.io/xwkrzaro" method="POST">
-        <input placeholder="Name" id="name" type="text" name="name" />
-
-        <input placeholder="Email" id="email" type="email" name="_replyto" />
-
-        <textarea placeholder="Message" id="message" name="message" className="wide"></textarea>
+        <div className="name-email">
+          <input placeholder="Name" id="name" type="text" name="name" />
+          <input placeholder="Email" id="email" type="email" name="_replyto" />
+        </div>
+        <textarea
+          placeholder="Message"
+          id="message"
+          name="message"
+          className="wide"
+        />
 
         <button type="submit">Send</button>
       </form>
